618f1029f429cc6152b1a804836d9c6106a3f602
penso996 <penso.andrea@outlook.it>
2025-03-12 17:59:33 +0100

    spacing

--- src/page_single_components/MovieCard.jsx
+++ src/page_single_components/MovieCard.jsx
@@ -11,11 +11,13 @@ const MovieCard = (props) => {
     return (
         <div className="movies-card">
             <h3>{HomePageMoviesData.title.toUpperCase()}</h3>
+
             {HomePageMoviesData.image && HomePageMoviesData.image !== "http://localhost:3000/movies_cover/" ? (
                 <img src={HomePageMoviesData.image} alt={HomePageMoviesData.title} />
             ) : (
                 <img src="/public/card_image_not_found.png" alt={HomePageMoviesData.title} />
             )}
+
             <NavLink to={`/movies/${HomePageMoviesData.id}`}>SCHEDA DEL FILM</NavLink>
         </div>
     );
