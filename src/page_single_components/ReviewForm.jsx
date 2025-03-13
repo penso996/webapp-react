86627ce1d27af3f015ba4b333b5ba811c619043a
penso996 <penso.andrea@outlook.it>
2025-03-13 19:00:45 +0100

    fixed some syntax

--- src/page_single_components/ReviewForm.jsx
+++ src/page_single_components/ReviewForm.jsx
@@ -1,7 +1,7 @@
 // ReviewForm.jsx
 
 // Import functions from React
-import { useState, useEffect } from "react";
+import { useState } from "react";
 // Import axios
 import axios from "axios";
 
@@ -10,18 +10,18 @@ const ReviewForm = (props) => {
     const { MovieDetailMovieId, fetchMovieData } = props;
 
     // Set initial form state (empty)
-    const initialFormState = { name: "", vote: "", text: "" }
+    const initialFormState = { name: "", vote: "", text: "" };
     // useState to handle formData
     const [formData, setFormData] = useState(initialFormState);
 
     // FUNCTION to handle input change
     const handleFormState = (e) => {
         const { name, value } = e.target;
-        setFormData({
-            ...formData,
+        setFormData(prevState => ({
+            ...prevState,
             [name]: value
-        })
-    }
+        }));
+    };
 
     // FUNCTION to send form data
     const handleSubmit = (e) => {
@@ -32,7 +32,7 @@ const ReviewForm = (props) => {
                 fetchMovieData();
             })
             .catch(err => console.error(err));
-    }
+    };
 
 
     // RENDER
@@ -40,13 +40,13 @@ const ReviewForm = (props) => {
         <div>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="name">NOME UTENTE</label>
-                <input type="text" name="name" value={formData.reviewName} onChange={handleFormState} />
+                <input type="text" name="name" value={formData.name} onChange={handleFormState} />
 
                 <label htmlFor="vote">VOTO</label>
-                <input type="number" min="1" max="5" name="vote" value={formData.reviewVote} onChange={handleFormState} />
+                <input type="number" min="1" max="5" name="vote" value={formData.vote} onChange={handleFormState} />
 
                 <label htmlFor="text">DESCRIZIONE</label>
-                <input type="text" name="text" value={formData.reviewText} onChange={handleFormState} />
+                <input type="text" name="text" value={formData.text} onChange={handleFormState} />
 
                 <button type="submit">invia</button>
             </form>
