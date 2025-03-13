2c7ff78b78103af86d71e26787e75e45a2a3b9b0
penso996 <penso.andrea@outlook.it>
2025-03-13 18:55:49 +0100

    working form for reveiws

--- src/page_single_components/ReviewForm.jsx
+++ src/page_single_components/ReviewForm.jsx
@@ -5,7 +5,9 @@ import { useState, useEffect } from "react";
 // Import axios
 import axios from "axios";
 
-const ReviewForm = () => {
+const ReviewForm = (props) => {
+
+    const { MovieDetailMovieId, fetchMovieData } = props;
 
     // Set initial form state (empty)
     const initialFormState = { name: "", vote: "", text: "" }
@@ -21,18 +23,32 @@ const ReviewForm = () => {
         })
     }
 
+    // FUNCTION to send form data
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post(`http://localhost:3000/movies/${MovieDetailMovieId}/reviews`, formData)
+            .then(() => {
+                setFormData(initialFormState);
+                fetchMovieData();
+            })
+            .catch(err => console.error(err));
+    }
+
+
     // RENDER
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="name">NOME UTENTE</label>
                 <input type="text" name="name" value={formData.reviewName} onChange={handleFormState} />
 
                 <label htmlFor="vote">VOTO</label>
-                <input type="text" name="vote" value={formData.reviewVote} onChange={handleFormState} />
+                <input type="number" min="1" max="5" name="vote" value={formData.reviewVote} onChange={handleFormState} />
 
                 <label htmlFor="text">DESCRIZIONE</label>
                 <input type="text" name="text" value={formData.reviewText} onChange={handleFormState} />
+
+                <button type="submit">invia</button>
             </form>
         </div>
     );
