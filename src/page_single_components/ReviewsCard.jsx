d547395cde6b0dec18eed4e3416b3b617988895a
penso996 <penso.andrea@outlook.it>
2025-03-14 12:46:55 +0100

    removed useless const

--- src/page_single_components/ReviewsCard.jsx
+++ src/page_single_components/ReviewsCard.jsx
@@ -3,18 +3,17 @@
 const ReviewsCard = (props) => {
 
     const { MovieDetailReviewsData } = props;
-    const reviewVote = MovieDetailReviewsData.vote;
 
     // RENDER
     return (
         <div className="review-card">
             <h3>{MovieDetailReviewsData.name}</h3>
             <div>
-                <i className="fa-solid fa-star-of-life" style={{ color: reviewVote >= 1 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
-                <i className="fa-solid fa-star-of-life" style={{ color: reviewVote >= 2 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
-                <i className="fa-solid fa-star-of-life" style={{ color: reviewVote >= 3 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
-                <i className="fa-solid fa-star-of-life" style={{ color: reviewVote >= 4 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
-                <i className="fa-solid fa-star-of-life" style={{ color: reviewVote >= 5 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
+                <i className="fa-solid fa-star-of-life" style={{ color: MovieDetailReviewsData.vote >= 1 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
+                <i className="fa-solid fa-star-of-life" style={{ color: MovieDetailReviewsData.vote >= 2 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
+                <i className="fa-solid fa-star-of-life" style={{ color: MovieDetailReviewsData.vote >= 3 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
+                <i className="fa-solid fa-star-of-life" style={{ color: MovieDetailReviewsData.vote >= 4 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
+                <i className="fa-solid fa-star-of-life" style={{ color: MovieDetailReviewsData.vote >= 5 ? "#f39c12" : "rgba(0, 0, 0, 0.3)" }} />
             </div>
             <p>{MovieDetailReviewsData.text || "L'utente non ha scritto una recensione"}</p>
         </div>
